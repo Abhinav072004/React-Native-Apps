31f6665c1aeb9b21f4288c83b9df48723362b121
Abhinav Singh <abhinavsingh.as1507@gmail.com>
2025-08-15 22:52:36 +0530

    Bgchangerapp-file

--- index.js
+++ index.js
@@ -3,8 +3,9 @@
  */
 
 import { AppRegistry } from 'react-native';
-import App from './App';
+// import App from './App';
 // import Apppro from './Apppro';
+import bgchangerapp from './bgchangerapp';
 import { name as appName } from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent(appName, () => bgchangerapp);
